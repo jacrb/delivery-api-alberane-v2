03fcbdff3736ed8fb6d30ea4584f92766b89ae43
alberane <alberanelucio@gmail.com>
2025-08-12 14:29:41 -0300

    Init

---
+++ src/main/java/com/deliverytech/delivery/model/Cliente.java
@@ -0,0 +1,29 @@
+package com.deliverytech.delivery.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+@Entity
+@Builder
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    
+    @Column(unique = true)
+    private String email;
+
+    @Builder.Default
+    private Boolean ativo = true;
+    
+    @Builder.Default
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+}
