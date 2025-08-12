5ea5f232816147c977f0069e5e64d83f2f555a1d
alberane <alberanelucio@gmail.com>
2025-08-12 15:49:02 -0300

    Models - Role, Usuario e Cliente

--- src/main/java/com/deliverytech/delivery/model/Usuario.java
+++ src/main/java/com/deliverytech/delivery/model/Usuario.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,20 +13,26 @@ import lombok.Builder;
 
 @Entity
 @Builder
-public class Cliente {
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @SuppressWarnings("unused")
-    private String nome;
-    
     @Column(unique = true)
     private String email;
 
+    private String senha;
+    
+    private String nome;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Builder.Default
     private Boolean ativo = true;
-    
+
     @Builder.Default
     private LocalDateTime dataCriacao = LocalDateTime.now();
+    
+    private Long restauranteId;
 }
