18a8aab53f3fa4687f478bf8afed0d24c7315a76
alberane <alberanelucio@gmail.com>
2025-08-18 14:37:07 -0300

    Refatora configuração do OpenAPI para a API de Delivery, ajustando informações de contato e versão

--- src/main/java/com/deliverytech/delivery/config/OpenApiConfig.java
+++ src/main/java/com/deliverytech/delivery/config/OpenApiConfig.java
@@ -1,23 +1,31 @@
 package com.deliverytech.delivery.config;
 
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-    
 
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI()
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
         .info(new Info()
-            .title("Delivery API - SEU_NOME")
-            .version("0.1.0")
-            .description("API para sistemas de Delivery")
+            .title("Delivery API")
+            .version("1.0")
+            .description("API para sistema de delivery")
             .contact(new Contact()
-                .name("Delivery Tech - Suporte")
-                .email("suporte@site.com.br")));
-    }
+                .name("Delivery Tech")
+                .email("contato@deliverytech.com")))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(new io.swagger.v3.oas.models.Components()
+            .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
+  }
 }
